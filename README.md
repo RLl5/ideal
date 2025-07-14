d7a9a15651cda78082e69c002452d52210d7f3f9
199 <2990756018@qq.com>
2025-07-14 21:13:53 +0800

    完整提交：补充上传 css,js,img等所有文件夹

---
+++ README.md
@@ -0,0 +1,35 @@
+﻿#[Personal Homepage - Web Front - End Course Assignment](https://github.com/RLl5/ideal/blob/master/README.md#Personal Homepage - Web Front - End Course Assignment)
+ 
+1. Introduction
+ 
+This is my personal homepage, a project completed as part of my web front - end development course assignment. It serves as a showcase of my efforts in learning front - end technologies, including HTML, CSS, and JavaScript.
+ 
+![播放器的插画](img/8a257e9ab039294e3f196202a97e054.jpg)
+
+2. Features
+ 
+- Music Playback: Incorporated a music player feature. You can click on the relevant interactive elements to play and pause music, and there is a custom playlist.
+ 
+- Multi - page Navigation: With pages like Home, About, Notes, and Contact, each page is designed to present different types of content.
+ 
+- Responsive Design: The layout is adapted for different devices, such as mobile phones and tablets, ensuring a good viewing experience across various screen sizes.
+ 
+- Customizable Elements: You can easily replace images in the  img  folder and music in the relevant music storage location (e.g., the  other  folder in my project structure) to personalize the homepage.
+
+3. How to Visit and Interact
+ 
+- Visit: Click on this link to access my personal homepage: [Personal Homepage Link] OvO.
+ 
+- Interact:
+ 
+- On the homepage, try clicking on the interactive elements related to music to play or pause the music.
+ 
+- Navigate through the different pages (Home, About, Notes, Contact) to explore the content.
+ 
+- If you want to customize, you can replace the images in the  img  folder and the music in the appropriate location, and then update the relevant references in the HTML and JavaScript code.
+ 
+4. Welcome to Provide Suggestions
+ 
+As a student's course assignment, there must be many areas for improvement. I sincerely welcome you to visit my personal homepage and provide suggestions from aspects such as design, user experience, code structure, etc. Your suggestions will be of great help to my learning and improvement in web front - end development. Whether it's about the layout aesthetics, the smoothness of interaction, or the optimization of code logic, please feel free to let me know.
+ 
+Thank you for your attention and support!
--- script.js
+++ script.js
@@ -1,1 +1,271 @@
-alert('欢迎访问！');
+let navbarItem = document.querySelectorAll('.item')
+let contentItem = document.querySelectorAll('.content')
+for (let i = 0; i < navbarItem.length; i++) {
+	navbarItem[i].addEventListener('click', function () {
+		for (let i = 0; i < navbarItem.length; i++) {
+			navbarItem[i].classList.remove('active');
+		}
+		for (let j = 0; j < contentItem.length; j++) {
+			contentItem[j].classList.add('hide');
+		}
+		this.classList.add('active')
+		if (i <= 3) {
+			contentItem[i].classList.remove('hide');
+		}
+		if (i > 3) {
+			contentItem[i - 4].classList.remove('hide');
+		}
+	})
+}
+for (let i = 1; i < contentItem.length; i++) {
+	contentItem[i].classList.add('hide');
+}
+
+
+var typed = new Typed(".multiple-text", {
+	strings: ['Music', 'Notes', 'Webs'],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop: true
+})
+
+// //监听页面尺寸变化，动态设置 html 下 font-size大小
+// window.onresize=() =>{
+// 	setHtmlFontSize();
+// }
+
+// function setHtmlFontSize() {
+// 	const clientWidth= document.body.clientWidth;
+// 	document.querySelector('html').style.fontSize=clientWidth /100+'px'
+// }
+
+// setHtmlFontSize();
+
+window.addEventListener('DOMContentLoaded', (event) => {
+	// 获取图片元素
+	var image = document.getElementById('myImage');
+
+	// 添加点击事件监听器
+	image.addEventListener('click', function () {
+		if (image.src.match('img/Jbdb45a534fd8845c0d4804d7049eaae.jpg')) {
+			audio.play();
+			playButton.classList.remove('bx-play');
+			playButton.classList.add('bx-pause');
+			image.src = 'img/bdb45a534fd8845c0d4804d7049eaae.jpg';
+		} else {
+			audio.pause();
+			playButton.classList.remove('bx-pause');
+			playButton.classList.add('bx-play');
+			image.src = 'img/bdb45a534fd8845c0d4804d7049eaae.jpg';
+		}
+	});
+
+	// 歌曲信息数组
+	const playlist = [{
+		id: '0',
+		title: 'R',
+		author: '李子豪',
+		path: 'https://music.163.com/song?id=1310728498',
+		time: "3:01"
+	},
+	{
+		id: '1',
+		title: '如果呢',
+		author: '郑润泽',
+		path: 'https://music.163.com/song?id=1842728629',
+		time: "4:17"
+	},
+	{
+		id: '2',
+		title: '独自',
+		author: '刘思鉴',
+		path: 'https://music.163.com/song?id=1451762098',
+		time: "3:09"
+	},
+	{
+		id: '3',
+		title: '哪里都是你2.0',
+		author: '队长',
+		path: 'https://music.163.com/song?id=1363642136',
+		time: "3:57"
+	}
+	];
+
+	//当前播放歌曲
+	let currentSongIndex = 0;
+
+	//初始化
+	function init() {
+		render(playlist[currentSongIndex]);
+	}
+
+	const playButton = document.getElementById('play');
+	const breadButton = document.getElementById('bread');
+	const audio = document.getElementById('audio');
+	const progressBar = document.getElementById('bar');
+	const progressOverlay = document.getElementById('overlay');
+	const startTime = document.getElementById('start');
+	const endTime = document.getElementById('end');
+	const title = document.getElementById('title');
+	const author = document.getElementById('author');
+	const previousButton = document.getElementById('previous');
+	const nextButton = document.getElementById('next');
+	const listButton = document.getElementById('list');
+	const songNum = document.getElementById('num');
+	const volumeButton = document.getElementById('volume');
+
+	const drop = document.querySelector('.drop');
+	breadButton.addEventListener('click', () => {
+		drop.classList.toggle('open');
+	});
+	document.addEventListener('click', (event) => {
+		const target = event.target;
+		const isBreadButton = target.closest('#bread');
+		const isDrop = target.closest('#drop');
+
+		if (!isBreadButton && !isDrop) {
+			drop.classList.remove('open');
+		}
+	});
+
+	//播放事件
+	playButton.addEventListener('click', () => {
+		if (audio.paused) {
+			playSong();
+		} else {
+			pauseSong();
+		}
+	});
+
+	//歌曲列表功能
+	const sidebar = document.querySelector('.sidebar');
+	// 生成歌曲列表项
+	function generatePlaylistItems(playlist) {
+		// 清空列表项
+		sidebarList.innerHTML = '';
+		// 遍历歌曲列表数据
+		var num = 0;
+		playlist.forEach((song) => {
+			// 创建列表项元素
+			const listItem = document.createElement('li');
+			listItem.textContent = song.title + '-' + song.author;
+			// 添加点击事件，播放对应的歌曲
+			listItem.addEventListener('click', () => {
+				currentSongIndex = song.id;
+				loadSong(currentSongIndex);
+				playSong();
+			});
+			// 将列表项添加到侧边栏列表中
+			sidebarList.appendChild(listItem);
+			num++;
+		});
+		songNum.innerHTML = num;
+	}
+	//调用函数生成歌曲列表项
+	generatePlaylistItems(playlist);
+	//播放列表按钮点击事件
+	listButton.addEventListener('click', toggleSidebar);
+
+	function toggleSidebar() {
+		sidebar.classList.toggle('open');
+	}
+
+	//播放完成自动播放下一首事件
+	audio.addEventListener('ended', function () {
+		// 增加当前歌曲索引
+		currentSongIndex++;
+		// 检查当前歌曲索引是否超出了播放列表的范围
+		if (currentSongIndex >= playlist.length) {
+			currentSongIndex = 0; // 如果超出范围，回到第一首歌曲
+		}
+		// 播放下一首歌曲
+		loadSong(currentSongIndex);
+		playSong();
+	});
+
+	//音量按钮点击事件
+	volumeButton.addEventListener('click', toggleVolume);
+
+	function toggleVolume() {
+		if (audio.volume === 0) {
+			audio.volume = 0.7; // 设置音量为默认值
+			volumeButton.classList.remove('bx-volume-mute'); // 移除静音图标类名
+			volumeButton.classList.add('bx-volume-full'); // 添加音量图标类名
+		} else {
+			audio.volume = 0; // 静音
+			volumeButton.classList.remove('bx-volume-full'); // 移除音量图标类名
+			volumeButton.classList.add('bx-volume-mute'); // 添加静音图标类名
+		}
+	}
+
+	//上一首事件
+	previousButton.addEventListener('click', () => {
+		previousSong();
+	});
+	//下一首事件
+	nextButton.addEventListener('click', () => {
+		nextSong();
+	});
+	//进度条功能
+	audio.addEventListener('timeupdate', () => {
+		const progress = (audio.currentTime / audio.duration) * 100;
+		startTime.textContent = formatTime(audio.currentTime);
+		//根据进度条调整颜色
+		progressOverlay.style.width = `${progress}%`;
+	});
+
+	function formatTime(time) {
+		const minutes = Math.floor(time / 60);
+		const seconds = Math.floor(time % 60);
+		return `${padZero(minutes)}:${padZero(seconds)}`;
+	}
+
+	function padZero(number) {
+		return number.toString().padStart(2, '0');
+	}
+	//上一首方法
+	function previousSong() {
+		currentSongIndex = (currentSongIndex - 1 + playlist.length) % playlist.length;
+		loadSong(currentSongIndex);
+		playSong();
+	}
+	//下一首方法
+	function nextSong() {
+		currentSongIndex = (currentSongIndex + 1) % playlist.length;
+		loadSong(currentSongIndex);
+		playSong();
+	}
+	//播放方法
+	function playSong() {
+		audio.play();
+		playButton.classList.remove('bx-play');
+		playButton.classList.add('bx-pause');
+		image.src = 'img/bdb45a534fd8845c0d4804d7049eaae.jpg';
+	}
+	//暂停方法
+	function pauseSong() {
+		audio.pause();
+		playButton.classList.remove('bx-pause');
+		playButton.classList.add('bx-play');
+		image.src = 'img/bdb45a534fd8845c0d4804d7049eaae.jpg';
+	}
+	//渲染歌曲方法
+	function render(song) {
+		title.innerHTML = song.title;
+		author.innerHTML = song.author;
+		endTime.innerHTML = song.time;
+		audio.volume = 0.7; //音量0~1;
+		audio.src = song.path; //音乐资源路径
+	}
+	//加载歌曲方法
+	function loadSong(index) {
+		const song = playlist[index];
+		title.innerHTML = song.title;
+		author.innerHTML = song.author;
+		endTime.innerHTML = song.time;
+		audio.src = song.path;
+	}
+
+	init();
+});
